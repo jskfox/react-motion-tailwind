516bbea91ca38337a65374ef27e9bdc05a7646d6
jskfox <solanokirk3@hotmail.com>
2024-11-17 15:03:20 -0800

    adding tailwindcss and modifying example layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,50 @@ import "./App.css";
 import viteLogo from "/vite.svg";
 
 function App() {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
 
   return (
     <>
       <motion.button layout 
-      className="button-switch p-5"
+      className="button-switch p-2 m-4"
       whileHover={{scale: 1.1,rotate:4}}
       whileTap={{scale: 0.9,rotate:-4}}
       onClick={() => setVisible(!visible)}>Animar</motion.button>
-      <div style={{minHeight:"30px"}}>
-        <a
-          href="#"
-          // target="_blank"
-        >
+      
+      <motion.button layout 
+      className="button-switch p-2 m-4"
+      whileHover={{scale: 1.1,rotate:4}}
+      whileTap={{scale: 0.9,rotate:-4}}
+      onClick={() => setVisible(!visible)}>Haz click!</motion.button>
+      
+      <div className="flex items-center justify-center h-screen">
+        
           <AnimatePresence mode="popLayout">
             {visible && (
               <motion.img
                 src={viteLogo}
-                className="logo"
+                className="logo size-56"
                 alt="Vite logo"
                 initial={{
                   rotate: 0,
-                  scale: 1,
+                  scale: 1.5,
                   x: 0,
                 }}
                 animate={{
                   rotate: [0,90,180,360],
                   x: [100, 20, 150,0 ],
-                  scale: [1, 1.5, 1.5, 1],
                 }}
                 transition={{
                   duration: 1.5,
                   type: "spring",
-                  //ease: "backInOut",
                   times: [0, 0.7, 0.8, 1],
                 }}
                 exit={{rotate:0, scale: .2, x:0, opacity:0}}
               />
             )}
           </AnimatePresence>
-        </a>
         
-          {/* <motion.img
-            layout
-            src={reactLogo}
-            // className="logo react"
-            alt="React logo"
-          /> */}      
       </div>
-      
     </>
   );
 }
