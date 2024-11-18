fbf6fc74aa4bfbfe40f1bbd90add7ac563023478
jskfox <solanokirk3@hotmail.com>
2024-11-18 01:12:55 -0800

    animations for useInView

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,115 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { AnimatePresence, motion, MotionConfig, useAnimationControls, useInView } from "framer-motion";
+import { useRef, useState } from "react";
 import "./App.css";
 import viteLogo from "/vite.svg";
 
 function App() {
   const [visible, setVisible] = useState(true);
+  const controls = useAnimationControls();
+  const ref = useRef();
+  const divIsInView = useInView(ref,{once:true});
   const css = {
     motionButton: 'button-switch p-2 m-4',
   }
+  const handleClick = () => {
+    controls.start("girar");
+    controls.set("inicial");
+  }
+
+  
   return (
     <>
-      <motion.button layout 
-      className={css.motionButton}
-      whileHover={{scale: 1.1,rotate:4}}
-      whileTap={{scale: 0.9,rotate:-4}}
-      onClick={() => setVisible(!visible)}>Animar</motion.button>
+      <MotionConfig transition={{ duration: 0.1, ease: "easeInOut" }}>
+        <motion.button
+          layout
+          className={css.motionButton}
+          whileHover={{ scale: 1.1, rotate: 4 }}
+          whileTap={{ scale: 0.9, rotate: -4 }}
+          onClick={() => setVisible(!visible)}
+        >
+          Animar
+        </motion.button>
+
+        <motion.button
+          layout
+          className={css.motionButton}
+          whileHover={{ scale: 1.1, rotate: 4 }}
+          whileTap={{ scale: 0.9, rotate: -4 }}
+          onClick={() => setVisible(!visible)}
+          transition={{ duration: 0.3, ease: "linear" }} // Slower than the global transition in MotionConfig
+        >
+          Haz click!
+        </motion.button>
+      </MotionConfig>
       
-      <motion.button layout 
-      className={css.motionButton}
-      whileHover={{scale: 1.1,rotate:4}}
-      whileTap={{scale: 0.9,rotate:-4}}
-      onClick={() => setVisible(!visible)}>Haz click!</motion.button>
+      <button onClick={handleClick}>Girar!</button>
+
+      <motion.div 
+        className="flex items-center justify-center p-4 bg-slate-200 w-72 h-screen rounded-lg shadow-lg text-gray-800"
+        variants={{
+          inicial: {rotate: 0},
+          girar: {rotate: 360},
+        }}
+        initial="inicial"
+        animate={controls}             
+        >
+            Contenido
+      </motion.div>
       
-      <div className="flex items-center justify-center h-screen">
-        
-          <AnimatePresence mode="popLayout">
-            {visible && (
-              <motion.img
-                src={viteLogo}
-                className="logo size-56"
-                alt="Vite logo"
-                initial={{
-                  rotate: 0,
-                  scale: 1.5,
-                  x: 0,
-                }}
-                animate={{
-                  rotate: [0,90,180,360],
-                  x: [100, 20, 150,0 ],
-                }}
-                transition={{
-                  duration: 1.5,
-                  type: "spring",
-                  times: [0, 0.7, 0.8, 1],
-                }}
-                exit={{rotate:0, scale: .2, x:0, opacity:0}}
-              />
-            )}
-          </AnimatePresence>
-        
+      <div className="flex items-center justify-center h-72">
+        <AnimatePresence mode="popLayout">          
+          {visible && (
+            <motion.img
+              src={viteLogo}
+              className="logo size-32"
+              alt="Vite logo"
+              initial={{
+                rotate: 0,
+                scale: 1.5,
+                x: 0,
+              }}
+              animate={{
+                rotate: [0, 90, 180, 360],
+                x: [100, 20, 150, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                type: "spring",
+                times: [0, 0.7, 0.8, 1],
+              }}
+              exit={{ rotate: 0, scale: 0.2, x: 0, opacity: 0 }}
+            />
+          )}
+        </AnimatePresence>
       </div>
+      
+      <motion.div className="h-screen w-96  bg-cyan-600"
+        initial={{
+          opacity:0,
+          // scale:0.5,
+          // x:800
+        }}
+        transition={{duration:0.5}}
+        whileInView={{
+          opacity:1, 
+          // scale:0.5,
+          // x:0,
+          // type: "spring",
+        }}
+      >
+      </motion.div>
+      
+      <motion.div className={`transition-colors duration-1000 h-screen w-96 ${!divIsInView ? "bg-green-400":"bg-orange-400"} `}
+        ref={ref}
+        initial={{
+          opacity:1,
+        }}
+        transition={{duration:1}}
+        whileInView={{
+          opacity:1, 
+        }}
+      >
+      </motion.div>
     </>
   );
 }
